b8b891a78deacf26658eb7d3fb58952b2cfc5372
anjiboddupally <boddupally.anji@gmail.com>
2023-06-15 21:10:54 +0100

    Authenticating ccfc fraud api

--- src/main/java/com/anji/finance/savecc/fraud/FraudClient.java
+++ src/main/java/com/anji/finance/savecc/fraud/FraudClient.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -14,6 +18,12 @@ public class FraudClient implements IFraudClient {
 
 	@Value("${ccfc.url}")
 	private String ccFcUrl;
+	
+	@Value("${ccfc.username}")
+	private String ccFcUsername;
+	
+	@Value("${ccfc.password}")
+	private String ccFcPassword;
 	
 	private static final String PATH = "/v1/fraudcheck/score";
 
@@ -26,16 +36,27 @@ public class FraudClient implements IFraudClient {
 		        .encode()
 		        .toUriString();
 
-
+		HttpHeaders headers = new HttpHeaders();
+		headers.setBasicAuth(ccFcUsername, ccFcPassword);
+		
+		HttpEntity<?> request = new HttpEntity<>(headers);
+		
 		RestTemplate restTemplate = new RestTemplate();
 
 		Map<String, String> params = new HashMap<>();
 		params.put("cc", ccNo);
 		params.put("exp", expDate.toString());
+		
+		
+		ResponseEntity<CCFraudInfo> response = restTemplate.exchange(
+				urlTemplate,
+		        HttpMethod.GET,
+		        request,
+		        CCFraudInfo.class);
 
-		CCFraudInfo result = restTemplate.getForObject(urlTemplate, CCFraudInfo.class);
+		//CCFraudInfo result = restTemplate.getForObject(urlTemplate, request, CCFraudInfo.class);
 
-		return result;
+		return response.getBody();
 	}
 
 }
